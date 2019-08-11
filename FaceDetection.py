a86e9b737ec8640574bf8213be09a8e0962552f8
nunzio03 <nunzio03@hotmail.it>
2019-08-11 14:18:34 +0200

    Added first position control commands ( UP DOWN )

--- FaceDetection.py
+++ FaceDetection.py
@@ -3,7 +3,7 @@ import sys
 from djitellopy import Tello
 
 TOLLERANCE_X = 80
-TOLLERANCE_Y = 40
+TOLLERANCE_Y = 20
 
 cascPath = sys.argv[1]  # percorso modello da rilevare
 faceCascade = cv2.CascadeClassifier(cascPath)
@@ -33,36 +33,40 @@ while True:
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
-
+    i = 0
     # Decorazione immagini per segnalare le facce
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)  # rettangolo di contorno
+        color = (255, 0, 0)
+        if i != 0:
+            color = (0, 0, 255)
+
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color, 5)  # rettangolo di contorno
         cv2.circle(frame, (int(x+w/2), int(y+h/2)), 12, (255, 0, 0), 1)  # cerchio di mira
         # print(frame.shape)
         # cv2.line(frame, (int(x+w/2), int(720/2)), (int(960/2), int(720/2)), (0, 255, 255))
 
         cv2.circle(frame, (int(960/3), int(720/3)), 12, (255, 255, 0), 8)  # cerchio di mira
-
+        i = i+1
         distanceX = x+w/2 - 960/3
         distanceY = y+h/2 - 720/3
 
-        if distanceX < -TOLLERANCE_X:
-            print("sposta il drone alla sua SX")
-        elif distanceX > TOLLERANCE_X:
-            print("sposta il drone alla sua DX")
-        else:
-            print("OK")
+        #if distanceX < -TOLLERANCE_X:
+        #    print("sposta il drone alla sua SX")
+        #elif distanceX > TOLLERANCE_X:
+        #    print("sposta il drone alla sua DX")
+        #else:
+        #    print("OK")
 
         if distanceY < -TOLLERANCE_Y:
-            print("sposta il drone in BASSO")
+            print("sposta il drone in ALTO")
             drone.move_up(20)
         elif distanceY > TOLLERANCE_Y:
-            print("sposta il drone in ALTO")
-            drone.move_up(25)
+            print("sposta il drone in BASSO")
+            drone.move_down(25)
 
         else:
             print("OK")
-    drone.send_rc_control()
+
     cv2.imshow('Video', frame)  # mostra il frame sul display del pc
 
     if cv2.waitKey(1) & 0xFF == ord('q'):  # quit from script
