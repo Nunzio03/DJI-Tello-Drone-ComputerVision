2bbf7abeab53890ace7a3285818fd3b5abe2d8fc
nunzio03 <nunzio03@hotmail.it>
2019-08-13 15:28:44 +0200

    code clean up

--- FaceDetection.py
+++ FaceDetection.py
@@ -8,26 +8,30 @@ SLOWDOWN_THRESHOLD_X = 20
 SLOWDOWN_THRESHOLD_Y = 20
 DRONE_SPEED_X = 20
 DRONE_SPEED_Y = 40
-cascPath = sys.argv[1]  # percorso modello da rilevare
+SET_POINT_X = 960/3
+SET_POINT_Y = 720/3
+
+
+cascPath = sys.argv[1]  # Path of the model used to reveal faces
 faceCascade = cv2.CascadeClassifier(cascPath)
-drone = Tello()  # dichiaro l'oggetto drone
+drone = Tello()  # declaring drone object
 drone.connect()
 drone.takeoff()
 
 
-drone.streamon()  # attivo lo streaming
+drone.streamon()  # start camera streaming
 
 
-# video_capture = cv2.VideoCapture("udp://0.0.0.0:11111")  #per video crudo
-# video_capture = cv2.VideoCapture("rtsp://192.168.1.1")  #per video da action cam
-# video_capture = cv2.VideoCapture(0)  # per video da webcam
+# video_capture = cv2.VideoCapture("udp://0.0.0.0:11111")  # raw video from drone streaming address
+# video_capture = cv2.VideoCapture("rtsp://192.168.1.1")  #raw video from action cam Apeman
+# video_capture = cv2.VideoCapture(0)  # raw video from webcam
 
 while True:
-    # lettura frame per frame
-    # ret, frame = video_capture.read()
+    # loop through frames
+    # ret, frame = video_capture.read()  # used to collect frame from alternative video streams
 
-    frame = drone.get_frame_read().frame  # cattura frame dal drone
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # immagine in scala di grigi
+    frame = drone.get_frame_read().frame  # capturing frame from drone
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # turning image into gray scale
 
     faces = faceCascade.detectMultiScale(  # face detection
         gray,
@@ -37,21 +41,21 @@ while True:
         flags=cv2.CASCADE_SCALE_IMAGE
     )
     i = 0
-    # Decorazione immagini per segnalare le facce
+    # Decorating image for debug purposes and looping through every detected face
     for (x, y, w, h) in faces:
         color = (255, 0, 0)
         if i != 0:
             color = (0, 0, 255)
 
-        cv2.rectangle(frame, (x, y), (x+w, y+h), color, 5)  # rettangolo di contorno
-        cv2.circle(frame, (int(x+w/2), int(y+h/2)), 12, (255, 0, 0), 1)  # cerchio di mira
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color, 5)  # contour rectangle
+        cv2.circle(frame, (int(x+w/2), int(y+h/2)), 12, (255, 0, 0), 1)  # face-centered circle
         # print(frame.shape)
         # cv2.line(frame, (int(x+w/2), int(720/2)), (int(960/2), int(720/2)), (0, 255, 255))
 
-        cv2.circle(frame, (int(960/3), int(720/3)), 12, (255, 255, 0), 8)  # cerchio di mira
+        cv2.circle(frame, (int(SET_POINT_X), int(SET_POINT_Y)), 12, (255, 255, 0), 8)  # setpoint circle
         i = i+1
-        distanceX = x+w/2 - 960/3
-        distanceY = y+h/2 - 720/3
+        distanceX = x+w/2 - SET_POINT_X
+        distanceY = y+h/2 - SET_POINT_Y
 
         up_down_velocity = 0
         right_left_velocity = 0
