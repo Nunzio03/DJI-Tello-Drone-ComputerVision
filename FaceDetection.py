87ba9d4282fa1c4a8ab6a499c766277a44a0444a
nunzio03 <nunzio03@hotmail.it>
2019-08-14 15:13:46 +0200

    Added PID features, fixed types of variables by casting

--- FaceDetection.py
+++ FaceDetection.py
@@ -3,16 +3,16 @@ import sys
 from djitellopy import Tello
 import time
 
-Kpx = 1
+Kpx = 0
 Kix = 0
 Kdx = 0
-Kpy = 1
+Kpy = 2
 Kiy = 0
-Kdy = 0
+Kdy = 1
 
-DELAY = 0.02
-SET_POINT_X = 640 / 2
-SET_POINT_Y = 480 / 2
+DELAY = 0.002
+SET_POINT_X = 960 / 2
+SET_POINT_Y = 720 / 2
 
 previous_error_x = 0
 integral_x = 0
@@ -31,13 +31,14 @@ drone.streamon()  # start camera streaming
 
 # video_capture = cv2.VideoCapture("udp://0.0.0.0:11111")  # raw video from drone streaming address
 # video_capture = cv2.VideoCapture("rtsp://192.168.1.1")  #raw video from action cam Apeman
-video_capture = cv2.VideoCapture(0)  # raw video from webcam
+# video_capture = cv2.VideoCapture(0)  # raw video from webcam
 
 while True:
     # loop through frames
-    ret, frame = video_capture.read()  # used to collect frame from alternative video streams
 
-    # frame = drone.get_frame_read().frame  # capturing frame from drone
+    # ret, frame = video_capture.read()  # used to collect frame from alternative video streams
+
+    frame = drone.get_frame_read().frame  # capturing frame from drone
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # turning image into gray scale
 
     faces = faceCascade.detectMultiScale(  # face detection
@@ -66,20 +67,21 @@ while True:
         derivative_y = (error_y - previous_error_y) / DELAY
         output_x = Kpx * error_x + Kix * integral_x + Kdx * derivative_x
         output_y = Kpy * error_y + Kiy * integral_y + Kdy * derivative_y
-        bounded_output_x = 100 * output_x / SET_POINT_X
-        bounded_output_y = 100 * output_y / SET_POINT_Y
+        bounded_output_x = int(100 * output_x / SET_POINT_X)
+        bounded_output_y = int(100 * output_y / SET_POINT_Y)
         # outx:240 = mapx : 100
         previous_error_x = error_x
         previous_error_y = error_y
         print("output X : ", bounded_output_x)
         print("output Y : ", bounded_output_y)
-        # drone.send_rc_control(0, 0, up_down_velocity, 0)
+        drone.send_rc_control(bounded_output_x, 0, bounded_output_y, 0)
 
         time.sleep(DELAY)
 
     cv2.imshow('Video', frame)  # mostra il frame sul display del pc
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):  # quit from script
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # quit from
+        print(drone.get_battery())
         break
 
 # rilascio risorse
